ec4d6b38061083f7ddb73b205b98b02c7ac6cd36
Michael Pye <mail@mdpye.co.uk>
2015-11-30 15:00:55 +0000

    Test failing exec healthchecks actually fail

--- check_exec_test.go
+++ check_exec_test.go
@@ -19,12 +19,32 @@ func TestCheckExecValidate(t *testing.T) {
 
 func TestCheckExecExecute(t *testing.T) {
 	c := CheckExec{
-		Path: "echo",
-		Args: []string{"hello"},
+		Path: "/bin/true",
 	}
 
 	err := c.Execute(1 * time.Second)
 	if err != nil {
 		t.Fatal("unexpected error:", err)
+	}
+}
+
+func TestCheckExecExecuteNonZero(t *testing.T) {
+	c := CheckExec{Path: "/bin/false"}
+
+	err := c.Execute(1 * time.Second)
+	if err == nil {
+		t.Fatal("expected error, got nil")
+	}
+}
+
+func TestCheckExecExecuteTimeout(t *testing.T) {
+	c := CheckExec{
+		Path: "/bin/sleep",
+		Args: []string{"1"},
+	}
+
+	err := c.Execute(250 * time.Millisecond)
+	if err == nil {
+		t.Fatal("expected error, got nil")
 	}
 }
